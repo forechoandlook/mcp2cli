a716f8a3367a5f767f91150cc36e381aac802a9b
forechoandlook <zuowangyang@foxmail.com>
2026-04-17 15:57:47 +0800

    feat: support multi-arch in install script

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ set -e
 
 REPO="forechoandlook/mcp2cli"
 LATEST_TAG=$(curl -s https://api.github.com/repos/$REPO/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-VERSION_NUM=$(echo $LATEST_TAG | sed 's/^v//')
 
 OS="linux"
 if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -12,12 +11,19 @@ elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
     OS="windows"
 fi
 
-BINARY_NAME="mcp2cli-$OS"
+ARCH=$(uname -m)
+case $ARCH in
+    x86_64) ARCH_NAME="x86_64" ;;
+    arm64|aarch64) ARCH_NAME="arm64" ;;
+    *) ARCH_NAME="x86_64" ;;
+esac
+
+BINARY_NAME="mcp2cli-$OS-$ARCH_NAME"
 if [[ "$OS" == "windows" ]]; then
     BINARY_NAME="$BINARY_NAME.exe"
 fi
 
-echo "Installing mcp2cli $LATEST_TAG for $OS..."
+echo "Installing mcp2cli $LATEST_TAG for $OS ($ARCH_NAME)..."
 URL="https://github.com/$REPO/releases/download/$LATEST_TAG/$BINARY_NAME"
 
 curl -L -o mcp2cli_tmp "$URL"
@@ -27,8 +33,10 @@ if [[ "$OS" == "windows" ]]; then
     mv mcp2cli_tmp mcp2cli.exe
     echo "Installed to current directory as mcp2cli.exe"
 else
+    mkdir -p ~/.local/bin
     mv mcp2cli_tmp ~/.local/bin/mcp2cli
     echo "Installed to ~/.local/bin/mcp2cli"
+    echo "Make sure ~/.local/bin is in your PATH."
 fi
 
 echo "Installation complete. Try running: mcp2cli list"
